c30d537b76e78b78d5195dbc20b6c52a79180078
Iryna Shchavlynska <134052549+ira-01@users.noreply.github.com>
2024-07-02 18:13:17 +0200

    Corrected_task2_second_edit

--- Homework_7_Java_Operators_Conditions/JavaOperatorsAndConditions/src/Task2_CoffeeShop.java
+++ Homework_7_Java_Operators_Conditions/JavaOperatorsAndConditions/src/Task2_CoffeeShop.java
@@ -5,11 +5,28 @@ public class Task2_CoffeeShop {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введіть кількість еспресо: ");
-        int espresso = scanner.nextInt(); // запам*ятовуємо скільки чашок еспресо
+        int espresso = scanner.nextInt();
+
+        if (espresso <= 0) {
+            System.out.println("Введені некоректні дані, дані автоматично зміняться на додатні");
+            espresso = espresso*(-1);
+        }
+
         System.out.println("Введіть кількість лате: ");
-        int latte = scanner.nextInt(); // скільки лате
+        int latte = scanner.nextInt();
+
+        if (latte <= 0) {
+            System.out.println("Введені некоректні дані, дані автоматично зміняться на додатні");
+            latte = latte*(-1);
+        }
+
         System.out.println("Введіть кількість капучіно: ");
-        int cappuccino = scanner.nextInt();  // скільки капучино
+        int cappuccino = scanner.nextInt();
+
+        if (cappuccino < 0) {
+            System.out.println("Введені некоректні дані, дані автоматично зміняться на додатні");
+            cappuccino = cappuccino*(-1);
+        }
 
         int price = espresso * 3 + latte * 4 + cappuccino * 5;
         System.out.println("Загальна сума замовлення =" +price +"$");
